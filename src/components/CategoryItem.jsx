1ac90cb2806a2225aff88f7c44e1128366c5cdc5
Umar <nurbekatajonov67@gmail.com>
2022-03-19 01:02:15 +0500

    1kun navbar slider ishladim

---
+++ src/components/CategoryItem.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const Container = styled.div`
+
+`
+
+
+const CategoryItem = ({item}) => {
+  return (
+    <Container>
+
+    </Container>
+  )
+}
+
+export default CategoryItem
