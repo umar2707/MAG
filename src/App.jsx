ef8b12f5f4d014f98265fcf72f2122dfe024b210
Umar <nurbekatajonov67@gmail.com>
2022-03-24 13:21:32 +0500

    prevv win

--- src/App.jsx
+++ src/App.jsx
@@ -4,9 +4,21 @@ import ProductList from "./pages/ProductList";
 import Register from "./pages/Register";
 import Login from './pages/Login';
 import Cart from './pages/Cart';
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 
 const App = () => {
-  return <Home />;
+  return(
+    <Router>
+      <Switch>
+          <Route exact path="/">
+            <Home />  
+          </Route>
+          <Route path="/products/:category">
+            <ProductList />  
+          </Route> 
+        </Switch>
+    </Router>
+  )
 };
 
 export default App;
