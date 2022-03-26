9eb687b7d7326481b7b0bf8fb04cc239ada45bb4
umar2707 <nurbekatajonov67@gmail.com>
2022-03-26 22:42:33 +0500

    l

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react'
+import React from 'react'
 import styled from 'styled-components'
 import Navbar from './../components/Navbar';
 import Announcement from './../components/Announcement';
