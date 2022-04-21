eaf2ac0df9423ccba5544575961cc47f91379aa6
umar2707 <nurbekatajonov67@gmail.com>
2022-04-21 17:47:32 +0500

    aaa

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react'
 import styled from 'styled-components'
 import Product from './Product';
-import axios from 'axios'
+import { publicRequest } from '../requestMethods';
 
 const Container = styled.div`
   padding:20px;
@@ -15,8 +15,8 @@ const Products = ({cat,filters,sort}) => {
   useEffect(()=>{
     const getProducts = async ()=>{
       try{
-        const res = await axios.get(cat ? `http://localhost:5000/api/products?category=${cat}` 
-                                        : `http://localhost:5000/api/products`);
+        const res = await publicRequest.get(cat ? `/products?category=${cat}` 
+                                        : `/products`);
         setProducts(res.data)
       }catch(err){}
     }
